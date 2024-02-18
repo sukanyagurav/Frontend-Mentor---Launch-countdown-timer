57c72da48ebda178c9c690c2d28b7428650755f2
sukanyagurav <sukanyagurav6@gmail.com>
2024-02-18 13:00:37 +0530

    added date picker

--- script.js
+++ script.js
@@ -6,12 +6,22 @@ const lblMinutes = document.getElementById('minutes')
 const lblSeconds = document.getElementById('seconds')
 
 function clock(endDate)
-{
+{ const error = document.querySelector('.error')
     const end = new Date(endDate)
     const now = new Date()
+    if(end < now){
+       
+        error.textContent='Launch date should be future date.Please select correct date.'
+        error.style.display='block'
+    }
+    else{
+        error.style.display='none'
+    }
+
+   
     const diff  = (end - now) / 1000
     if(diff < 0 ){
-       retun
+       return
     }
     const days =Math.floor(diff/3600/24)
     const hours = Math.floor(diff / 3600) % 24
@@ -22,9 +32,12 @@ function clock(endDate)
     lblMinutes.textContent = String(minutes).length < 2 ? String(minutes).padStart(2,0) : minutes
     lblSeconds.textContent = String(seconds).length < 2 ? String(seconds).padStart(2,0) : seconds
 }
-clock(endDate)
-
-setInterval(()=>{
-    clock(endDate)
-},1000)
-
+const dateField = document.getElementById('date-time')
+dateField.addEventListener('change',function(e){
+        
+    setInterval(()=>{
+        endDate =dateField.value
+        clock(endDate)
+    },1000)
+    
+})
