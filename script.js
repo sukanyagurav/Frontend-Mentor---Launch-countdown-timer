498940f48889245c863e43d01b7b11aa7ea48f7e
sukanyagurav <sukanyagurav6@gmail.com>
2024-02-18 16:04:33 +0530

    added popper

--- script.js
+++ script.js
@@ -4,25 +4,48 @@ const lbldays = document.getElementById('days')
 const lblHours = document.getElementById('hours')
 const lblMinutes = document.getElementById('minutes')
 const lblSeconds = document.getElementById('seconds')
-
+const error = document.querySelector('.error')
+let interval
 function clock(endDate)
-{ const error = document.querySelector('.error')
+{ 
     const end = new Date(endDate)
     const now = new Date()
-    if(end < now){
-       
-        error.textContent='Launch date should be future date.Please select correct date.'
+    const diff  = (end - now) / 1000
+    
+  
+   
+    if(diff < 0 ){
         error.style.display='block'
-    }
-    else{
-        error.style.display='none'
-    }
+        error.textContent='Event Started!!!'
+        const svgContainer = document.createElement('div')
+        svgContainer.classList.add('svg')
+        const time = setTimeout(()=>{
+
+            document.querySelector('.launchdown-input').appendChild(svgContainer)
+             const animItem = bodymovin.loadAnimation({
+                 wrapper: svgContainer,
+                 animType: 'svg',
+                 loop: false,
+                 autoplay: false,
+              path:'https://lottie.host/bc0a77ee-fae9-4026-927f-e2547014a5f4/2dhgp8ALNK.json'
+             })
+             animItem.goToAndPlay(0,true)
+           
+        },1000)
+        
+        setTimeout(()=>{
+            clearTimeout(time)
+            clearInterval(interval)
+            document.querySelector('.launchdown-input').removeChild(svgContainer)
+            error.textContent='Enter new event countdown'
+          
+        },5000)
+        return
+     }
+    
 
    
-    const diff  = (end - now) / 1000
-    if(diff < 0 ){
-       return
-    }
+    
     const days =Math.floor(diff/3600/24)
     const hours = Math.floor(diff / 3600) % 24
     const minutes = Math.floor(diff /60)%60
@@ -34,10 +57,18 @@ function clock(endDate)
 }
 const dateField = document.getElementById('date-time')
 dateField.addEventListener('change',function(e){
-        
-    setInterval(()=>{
-        endDate =dateField.value
-        clock(endDate)
-    },1000)
+    const end = new Date(e.target.value)
+    const now = new Date()
+    if(end < now){
+        error.textContent='Launch date should be future date.Please select correct date.'
+        error.style.display='block'
+    }
+    else{
+        error.textContent=''
+        interval= setInterval(()=>{
+            endDate =dateField.value
+            clock(endDate)
+        },1000)
+    }
     
 })
